1e1422a9f06b2ac31d1c960277d387706c4fa528
David Lewis and Roger Yee <roger@>
2018-02-20 13:24:10 -0800

    write out app.rb to take in information from doctor form

--- app.rb
+++ app.rb
@@ -4,16 +4,20 @@ also_reload('lib/**/*.rb')
 require './lib/doctor.rb'
 require './lib/patient.rb'
 require 'pg'
+require 'pry'
 
 DB = PG.connect({:dbname => 'doctor_office'})
 
 get('/') do
-  @doctor_list = Doctor.all()
+  @doctors = Doctor.all()
   erb(:doctor)
 end
 
 post('/') do
-  new_task= Task.new(:description, :due_date)
-  new_task.save()
-  erb(:to_do)
+  name = params['doctor_name']
+  specialty = params['specialty']
+  binding.pry
+  new_doctor = Doctor.new({:name => name, :specialty => specialty, :id => 1})
+  new_doctor.save()
+  erb(:doctor)
 end
